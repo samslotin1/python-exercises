5425458042d9f19e2388510e1208c60cce9212ca
Sam Slotin <sam@example.com>
2026-01-24 14:19:58 +0200

    Add Python exercises

---
+++ boolean_logic_exercises.py
@@ -0,0 +1,40 @@
+first = "Hello World"
+
+print("I AM A COMPUTER!")
+
+if 1 < 2 and 4 > 2:
+    print("Math is fun.")
+
+nope = None
+
+result = True and False
+
+length = len("What's my length?")
+
+shouting = "i am shouting".upper()
+
+number = int("1000")
+
+combined = str(4) + "real"
+
+repeat = 3 * "cool"
+
+list_type = type([])
+
+name = input("What is your name? ")
+
+number = int(input("Enter a number: "))
+
+if number < 0:
+    print("That number is less than 0!")
+elif number > 0:
+    print("That number is greater than 0!")
+else:
+    print("You picked 0!")
+
+index_l = "apple".index("l")
+
+has_y = "y" in "xylophone"
+
+my_string = "hello world"
+is_lowercase = my_string.islower()
